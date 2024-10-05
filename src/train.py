c96775df06fa5addfb06661a92e6cee17bfbae68
ssavargaonkar <suhrud.savargaonkar@gmail.com>
2024-10-05 23:53:49 +0100

    Integrated Hydra | Added config files

--- src/train.py
+++ src/train.py
@@ -5,12 +5,55 @@ from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 from data import DataModule
 from model import ColaModel
 from pytorch_lightning.loggers import WandbLogger
+import hydra
+from omegaconf import OmegaConf
+import logging
+import wandb
+import pandas as pd
+
+logger = logging.getLogger(__name__)
 
 
+class SamplesVisualisationLogger(pl.Callback):
+    def __init__(self, datamodule):
+        super().__init__()
 
-def main():
-    cola_data = DataModule()
-    cola_model = ColaModel()
+        self.datamodule = datamodule
+
+    def on_validation_end(self, trainer, pl_module):
+        val_batch = next(iter(self.datamodule.val_dataloader()))
+        sentences = val_batch["sentence"]
+
+        outputs = pl_module(val_batch["input_ids"], val_batch["attention_mask"])
+        preds = torch.argmax(outputs.logits, 1)
+        labels = val_batch["label"]
+
+        df = pd.DataFrame(
+            {"Sentence": sentences, "Label": labels.numpy(), "Predicted": preds.numpy()}
+        )
+
+        wrong_df = df[df["Label"] != df["Predicted"]]
+        trainer.logger.experiment.log(
+            {
+                "examples": wandb.Table(dataframe=wrong_df, allow_mixed_types=True),
+                "global_step": trainer.global_step,
+            }
+        )
+
+
+@hydra.main(config_path="../configs",config_name="config.yaml")
+def main(cfg):
+
+
+    logger.info(OmegaConf.to_yaml(cfg,resolve=True))
+    logger.info(f"Using the model: {cfg.model.name}")   
+    logger.info(f"Using the tokenizer: {cfg.model.tokenizer}")
+
+
+    cola_data = DataModule(
+        cfg.model.tokenizer, cfg.processing.batch_size, cfg.processing.max_length
+    )
+    cola_model = ColaModel(cfg.model.name)
 
     # checkpoint_callback = ModelCheckpoint(
     #     dirpath="./models", monitor="val_loss", mode="min"
@@ -32,10 +75,14 @@ def main():
     trainer = pl.Trainer(
         default_root_dir="logs",
         gpus=(1 if torch.cuda.is_available() else 0),
-        max_epochs=5,
+        max_epochs=cfg.training.max_epochs,
         fast_dev_run=False,
         logger=wandb_logger,
-        callbacks=[checkpoint_callback, early_stopping_callback],
+        callbacks=[checkpoint_callback, SamplesVisualisationLogger(cola_data)],
+        log_every_n_steps = cfg.training.log_every_n_steps,
+        deterministic = cfg.training.deterministic,
+        limit_train_batches = cfg.training.limit_train_batches,
+        limit_val_batches = cfg.training.limit_val_batches
     )
     trainer.fit(cola_model, cola_data)
 
